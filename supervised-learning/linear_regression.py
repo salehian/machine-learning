53972572508052b8973cadae3989a362ad622e3c
salehian <salehian.ma@gmail.com>
2020-07-05 10:55:01 +0430

    adding cross_val_predict

--- supervised-learning/linear_regression.py
+++ supervised-learning/linear_regression.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 from sklearn import datasets
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
