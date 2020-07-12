10cb0a4c8770deae2466f4955b44da67570e4bb8
salehian <salehian.ma@gmail.com>
2020-07-12 12:37:12 +0430

    make predictions

--- supervised-learning/naive_bayes_classifier.py
+++ supervised-learning/naive_bayes_classifier.py
@@ -35,8 +35,12 @@ count_vector = CountVectorizer()
 training_data = count_vector.fit_transform(X_train)
 
 # Transform test data
-testing_data = count_vector.transform(X_test)
+test_data = count_vector.transform(X_test)
 
 # Train Multinomial Naive Bayes Classifier
 naive_bayes = MultinomialNB()
 naive_bayes.fit(training_data, y_train)
+
+# Make Predictions
+predictions = naive_bayes.predict(test_data)
+
