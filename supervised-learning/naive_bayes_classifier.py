c47f19577500f3b8caf1d44289e320dc69cc723d
salehian <salehian.ma@gmail.com>
2020-07-12 12:28:20 +0430

    applying bag of words

--- supervised-learning/naive_bayes_classifier.py
+++ supervised-learning/naive_bayes_classifier.py
@@ -20,3 +20,17 @@ X_train, X_test, y_train, y_test = train_test_split(data['sms_message'],
 
 # Print the first 5 rows
 print(data.head())
+
+# Line Separator
+line = 33*"="
+print(line.center(80))
+print('Number of rows in the data set: {}'.format(data.shape[0]))
+print('Number of rows in the training set: {}'.format(X_train.shape[0]))
+print('Number of rows in the test set: {}'.format(X_test.shape[0]))
+
+# Instantiate the CountVectorizer
+# It is used to convert a collection of text documents to a matrix of token counts
+count_vector = CountVectorizer()
+
+# Fit the training data
+training_data = count_vector.fit_transform(X_train)
