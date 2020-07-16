5c36c401a13367a01a52abef02dbccb9e69ad24f
salehian <salehian.ma@gmail.com>
2020-07-16 12:18:52 +0430

    sampling and scaling data

--- unsupervised-learning/kmeans.py
+++ unsupervised-learning/kmeans.py
@@ -1,13 +1,98 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from IPython.display import display
 
+from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
 # Load the wholesale customers dataset
 data = pd.read_csv("./data/customers.csv")
 data.drop(['Region', 'Channel'], axis = 1, inplace = True)
 print ("Wholesale customers dataset has {} samples with {} features each".format(*data.shape))
 
-display(data.describe())
+print(data.describe())
+
+# Select three indices to sample from the dataset
+indices = [60,110,160]
+
+# Create a DataFrame of the chosen samples
+samples = pd.DataFrame(data.loc[indices], columns = data.keys()).reset_index(drop = True)
+
+
+# Scale the data using the natural logarithm
+log_data = np.log(data)
+
+# Scale the sample data using the natural logarithm
+log_samples = np.log(samples)
+
+# Plot a scatter matrix for each pair of newly-transformed features
+pd.plotting.scatter_matrix(log_data, alpha = 0.3, figsize = (14,8), diagonal = 'kde');
+plt.show()
+
+
+all_outliers = np.array([], dtype='int64')
+
+# For each feature find the data points with extreme high or low values
+for feature in log_data.keys():
+    
+    # Calculate Q1 (25th percentile of the data) for the given feature
+    q1 = np.percentile(log_data[feature], 25)
+    
+    # Calculate Q3 (75th percentile of the data) for the given feature
+    q3 = np.percentile(log_data[feature], 75)
+    
+    # Use the interquartile range to calculate an outlier step (1.5 times the interquartile range)
+    step = (q3 - q1) * 1.5
+    
+    outlier_points = log_data[~((log_data[feature] >= q1 - step) & (log_data[feature] <= q3 + step))]
+    all_outliers = np.append(all_outliers, outlier_points.index.values.astype('int64'))
+    # Display the outliers
+    print ("Data points considered outliers for the feature '{}':".format(feature))
+    print(outlier_points)
+
+all_outliers, indices = np.unique(all_outliers, return_inverse=True)
+counts = np.bincount(indices)
+outliers = all_outliers[counts>1]
+
+# Remove the outliers, if any were specified
+good_data = log_data.drop(log_data.index[outliers]).reset_index(drop = True)
+
+
+# TODO: Apply PCA to the good data with the same number of dimensions as features
+pca = PCA(n_components=6).fit(good_data)
+
+# TODO: Apply a PCA transformation to the sample log-data
+pca_samples = pca.transform(log_samples)
+
+# Generate PCA results plot
+#pca_results = rs.pca_results(good_data, pca)
+
+# TODO: Fit PCA to the good data using only two dimensions
+pca = PCA(n_components=2).fit(good_data)
+
+# TODO: Apply a PCA transformation the good data
+reduced_data = pca.transform(good_data)
+
+# TODO: Apply a PCA transformation to the sample log-data
+pca_samples = pca.transform(log_samples)
+
+# Create a DataFrame for the reduced data
+reduced_data = pd.DataFrame(reduced_data, columns = ['Dimension 1', 'Dimension 2'])
+
+# display cumulative variance:
+#print (pca_results['Explained Variance'].cumsum())
+
+
+reduced_samples = pd.DataFrame(pca_samples, columns = ['Dimension 1', 'Dimension 2'])
+
+# TODO: Apply your clustering algorithm of choice to the reduced data 
+clusterer = KMeans(n_clusters=2, random_state=29).fit(reduced_data)
+# TODO: Predict the cluster for each data point
+preds = clusterer.predict(reduced_data)
+centers = clusterer.cluster_centers_
+# TODO: Predict the cluster for each transformed sample data point
+sample_preds = clusterer.predict(reduced_samples)
+# TODO: Calculate the mean silhouette coefficient for the number of clusters chosen
+score = silhouette_score(reduced_data, clusterer.labels_, metric='euclidean')
+print("KMeans score", score)
