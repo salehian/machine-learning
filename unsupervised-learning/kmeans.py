006ecc1db992841fb1884accb2fb895289949c6a
salehian <salehian.ma@gmail.com>
2020-07-16 10:39:51 +0430

    data exploration

--- unsupervised-learning/kmeans.py
+++ unsupervised-learning/kmeans.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from IPython.display import display
 
 from sklearn.cluster import KMeans
+
+# Load the wholesale customers dataset
+data = pd.read_csv("./data/customers.csv")
+data.drop(['Region', 'Channel'], axis = 1, inplace = True)
+print ("Wholesale customers dataset has {} samples with {} features each".format(*data.shape))
+
+display(data.describe())
