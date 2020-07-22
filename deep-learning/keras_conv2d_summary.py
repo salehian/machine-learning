14d1b01d02a3647698971594bd07a30e05069497
salehian <salehian.ma@gmail.com>
2020-07-22 14:59:15 +0430

    commenting the formula for the number of parameters in a conv2d

--- deep-learning/keras_conv2d_summary.py
+++ deep-learning/keras_conv2d_summary.py
@@ -5,3 +5,8 @@ model = Sequential()
 model.add(Conv2D(filters=16, kernel_size=2, strides=2, padding='valid',
                  activation='relu', input_shape=(200, 200, 1)))
 model.summary()
+
+# Formula: Number of Parameters in a Convolutional Layer
+# K - the number of filters in the convolutional layer
+# F - the height and width of the convolutional filters
+# D_in - the depth of the previous layer
