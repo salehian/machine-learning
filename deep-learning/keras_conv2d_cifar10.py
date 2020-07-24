a72a991a54de77a364c803b8eea66a5d58a489af
salehian <salehian.ma@gmail.com>
2020-07-24 16:13:12 +0430

    adding one-hot encoding and validation split

--- deep-learning/keras_conv2d_cifar10.py
+++ deep-learning/keras_conv2d_cifar10.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 import keras
 from keras.datasets import cifar10
+from keras.utils import np_utils
+
 
 # Load the pre-shuffled train and test data
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
@@ -18,3 +20,12 @@ for i in range(18):
 # Rescale
 X_train = X_train.astype('float32')/255
 y_train = y_train.astype('float32')/255
+
+# One-hot encode the labels
+num_classes = len(unique(y_train))
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# Break a part from training set for validation set
+(X_train, X_valid) = X_train[9000:], X_train[:9000]
+(y_train, y_valid) = y_train[9000:], y_train[:9000]
