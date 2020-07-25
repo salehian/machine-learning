fcbef6c859f5890737ee7ab50be88f7a824204e3
salehian <salehian.ma@gmail.com>
2020-07-25 07:51:47 +0430

    adding another set of conv2D and MaxPooling2D to the model

--- deep-learning/keras_conv2d_cifar10.py
+++ deep-learning/keras_conv2d_cifar10.py
@@ -41,3 +41,5 @@ model = Sequential()
 model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu',
 				 input_shape=(32, 32, 3)))
 model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
