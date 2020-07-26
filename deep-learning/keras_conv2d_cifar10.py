4e132badc16e1c20e679e5e90732e5b649e8cbc7
salehian <salehian.ma@gmail.com>
2020-07-26 15:16:23 +0430

    compiling the model

--- deep-learning/keras_conv2d_cifar10.py
+++ deep-learning/keras_conv2d_cifar10.py
@@ -13,16 +13,10 @@ from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 # Plot the first 18 images
 fig = plt.figure(figsize=(20,5))
 for i in range(18):
-  ax = fig.add_subplot(3, 6, i+1)
-  ax.set_xticks([])
-  ax.set_yticks([])
-  ax.imshow(np.squeeze(X_train[i]))
+    ax = fig.add_subplot(3, 6, i + 1, xticks=[], yticks=[])
+    ax.imshow(np.squeeze(X_train[i]))
 
 # Rescale
-X_train = X_train.astype('float32')/255
-y_train = y_train.astype('float32')/255
-
-# One-hot encode the labels
 num_classes = len(np.unique(y_train))
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
@@ -31,6 +25,8 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 (X_train, X_valid) = X_train[9000:], X_train[:9000]
 (y_train, y_valid) = y_train[9000:], y_train[:9000]
 
+
+
 # Print number of train, test, and validation image samples
 print('Train Samples : %d'%X_train.shape[0])
 print('Test Samples : %d'%X_test.shape[0])
@@ -38,15 +34,21 @@ print('Validation Samples : %d'%X_valid.shape[0])
 
 # Define the model
 model = Sequential()
-model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu',
-				 input_shape=(32, 32, 3)))
+model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu', 
+                        input_shape=(32, 32, 3)))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.2))
+model.add(Dropout(0.3))
 model.add(Flatten())
 model.add(Dense(500, activation='relu'))
 model.add(Dropout(0.4))
 model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', 
+                  metrics=['accuracy']) 
