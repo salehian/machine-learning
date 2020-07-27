65f46beda8f3d7cdac589989e06c3691fc26f79a
salehian <salehian.ma@gmail.com>
2020-07-27 16:40:57 +0430

    reinforcement learning with gym's cart-pole-v0

---
+++ reinforcement-learning/cart-pole-v0.py
@@ -0,0 +1,10 @@
+import numpy as np
+from collections import deque
+import matplot.pyplot as plt
+import gym
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as functional
+import torch.optim as optim
+from torch.distributions import Categorical
